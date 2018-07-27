29456021da6246f8b99a6d63f111f170cd3d0c42
Adam Argyle <atom@argyleink.com>
2018-07-26 20:04:34 -0700

    fixes multiple launches causing multiple inits of tool

--- extension/pixelbug.js
+++ extension/pixelbug.js
@@ -1,7 +1,13 @@
+let has_init = false
+
 chrome.browserAction.onClicked.addListener(function (tab) {
+  if (has_init) return
+
   chrome.tabs.insertCSS(tab.id, { file: 'toolbar/bundle.css' })
 
   chrome.tabs.executeScript(tab.id, { file: 'web-components.polyfill.js' })
   chrome.tabs.executeScript(tab.id, { file: 'toolbar/bundle.js' })
   chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
+
+  has_init = true
 })
