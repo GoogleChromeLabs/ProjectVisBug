91fc420b8cc094b881ae3dbdda9602fd68f9518e
Adam Argyle <atom@argyleink.com>
2018-07-26 20:28:21 -0700

    revert, less problems this way

--- extension/pixelbug.js
+++ extension/pixelbug.js
@@ -1,13 +1,7 @@
-let init_map = {}
-
 chrome.browserAction.onClicked.addListener(function (tab) {
-  if (init_map[tab.id]) return
-
   chrome.tabs.insertCSS(tab.id, { file: 'toolbar/bundle.css' })
 
   chrome.tabs.executeScript(tab.id, { file: 'web-components.polyfill.js' })
   chrome.tabs.executeScript(tab.id, { file: 'toolbar/bundle.js' })
   chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
-
-  init_map[tab.id] = true
 })
