d59e3c3f607e8b1bc7c2cc93dae937ee573d51f0
Adam Argyle <atom@argyleink.com>
2018-07-27 20:50:10 -0700

    start filtering down what designers may want

--- app/features/metatip.js
+++ app/features/metatip.js
@@ -1,19 +1,24 @@
 import $ from 'blingblingjs'
+import { getStyles } from './utils'
 
 const template = ({target: el, pageX, pageY}) => {
   const { width, height } = el.getBoundingClientRect()
+  const styles = getStyles(el)
   let metatip = document.createElement('div')
 
   metatip.classList.add('metatip')
   metatip.style = `
-    top: ${pageY + 75}px;
-    left: ${pageX + 75}px;
+    top: ${pageY + 25}px;
+    left: ${pageX + 25}px;
   `
   metatip.innerHTML = `
     <h5>${el.nodeName.toLowerCase()}${el.id && '#' + el.id}${el.className && '.'+el.className.replace(/ /g, '.')}</h5>
-    <small>${Math.round(width)}px x ${Math.round(height)}px</small>
+    <small>${Math.round(width)}px × ${Math.round(height)}px</small>
+    <div>${styles.reduce((items, item) => `
+      ${items}
+      <span prop>${item.prop}:</span><span value>${item.value};</span>
+    `, '')}</div>
   `
-  // <ol>styles_list</ol>
 
   return metatip
 }
@@ -26,8 +31,8 @@ export function MetaTip() {
 
     if (tip_map[e.target]) {
       const tip = tip_map[e.target]
-      tip.style.top = `${e.pageY + 75}px`
-      tip.style.left = `${e.pageX + 75}px`
+      tip.style.top = `${e.pageY + 25}px`
+      tip.style.left = `${e.pageX + 25}px`
       return
     }
 
--- app/features/utils.js
+++ app/features/utils.js
@@ -5,16 +5,42 @@ export function getSide(direction) {
   return start
 }
 
-export function getStyle(elem, name) {
+export function getStyle(el, name) {
   if (document.defaultView && document.defaultView.getComputedStyle) {
     name = name.replace(/([A-Z])/g, '-$1')
     name = name.toLowerCase()
-    let s = document.defaultView.getComputedStyle(elem, '')
+    let s = document.defaultView.getComputedStyle(el, '')
     return s && s.getPropertyValue(name)
   } 
   else {
     return null
   }
+}
+
+const desiredPropMap = {
+  padding:        '0px',
+  margin:         '0px',
+  fontSize:       '16px',
+  fontWeight:     '400',
+  lineHeight:     'normal',
+  display:        'block',
+  alignItems:     'normal',
+  justifyContent: 'normal',
+}
+export function getStyles(el) {
+  const elStyleObject = el.style
+  const computedStyle = window.getComputedStyle(el, null)
+
+  let desiredValues = []
+
+  for (prop in el.style)
+    if (prop in desiredPropMap && desiredPropMap[prop] != computedStyle[prop])
+      desiredValues.push({
+        prop,
+        value: computedStyle[prop]
+      })
+
+  return desiredValues
 }
 
 let timeoutMap = {}
