d3a1de8478c507b4b497ceb354cdc1639459e965
Adam Argyle <atom@argyleink.com>
2018-09-12 22:41:14 -0700

    fixes #10

--- app/features/hueshift.js
+++ app/features/hueshift.js
@@ -23,16 +23,16 @@ export function HueShift(Color) {
       , keys = handler.key.split('+')
 
     keys.includes('left') || keys.includes('right')
-      ? changeHue(selectedNodes, keys, 's', this.active_color)
-      : changeHue(selectedNodes, keys, 'l', this.active_color)
+      ? changeHue(selectedNodes, keys, 's', Color)
+      : changeHue(selectedNodes, keys, 'l', Color)
   })
 
   hotkeys(command_events, (e, handler) => {
     e.preventDefault()
     let keys = handler.key.split('+')
     keys.includes('left') || keys.includes('right')
-      ? changeHue($('[data-selected=true]'), keys, 'a', this.active_color)
-      : changeHue($('[data-selected=true]'), keys, 'h', this.active_color)
+      ? changeHue($('[data-selected=true]'), keys, 'a', Color)
+      : changeHue($('[data-selected=true]'), keys, 'h', Color)
   })
 
   hotkeys('[,]', (e, handler) => {
@@ -59,14 +59,14 @@ export function HueShift(Color) {
   }
 }
 
-export function changeHue(els, direction, prop, active_color) {
+export function changeHue(els, direction, prop, Color) {
   els
     .map(el => showHideSelected(el))
     .map(el => {
       const { foreground, background } = extractPalleteColors(el)
 
       // todo: teach hueshift to do handle color
-      switch(active_color) {
+      switch(Color.getActive()) {
         case 'background':
           return { el, current: background.color.toHsl(), style: background.style }
         case 'foreground':
@@ -93,8 +93,13 @@ export function changeHue(els, direction, prop, active_color) {
 
       return payload
     })
-    .forEach(({el, style, current}) =>
-      el.style[style] = new TinyColor(current).setAlpha(current.a).toHslString())
+    .forEach(({el, style, current}) => {
+      let color = new TinyColor(current).setAlpha(current.a)
+      el.style[style] = color.toHslString()
+
+      if (style == 'color') Color.foreground.color(color.toHexString())
+      if (style == 'backgroundColor') Color.background.color(color.toHexString())
+    })
 }
 
 export function extractPalleteColors(el) {
