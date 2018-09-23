9edea432f20f55a061597aed78b96b9a26f50093
Adam Argyle <atom@argyleink.com>
2018-09-23 15:32:25 -0700

    adding more inspirational prototype areas

--- extension/panel/index.js
+++ extension/panel/index.js
@@ -1,1 +1,18 @@
-chrome.devtools.panels.create('Design', null, 'panel/panel.html')
+chrome.devtools.panels.create('Design', null, 'panel/panel.html', design_panel => {
+  console.info(chrome.devtools.panels.themeName)
+})
+
+chrome.devtools.panels.elements.createSidebarPane("PixelBug", sidebar => {
+  sidebar.setPage("../pane/pixelbug/index.html");
+  sidebar.setHeight("8ex");
+})
+
+chrome.devtools.panels.elements.createSidebarPane("Style Guide", sidebar => {
+  sidebar.setPage("../pane/styleguide/index.html");
+  sidebar.setHeight("8ex");
+})
+
+chrome.devtools.panels.elements.createSidebarPane("Sizing", sidebar => {
+  sidebar.setPage("../pane/sizing/index.html");
+  sidebar.setHeight("8ex");
+})
