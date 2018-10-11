073736496383d89c2966c1200770cb22e0631bfd
Adam Argyle <atom@argyleink.com>
2018-10-11 05:40:36 -0700

    control is now the key to hide selection borders to not conflict with other hotkeys

--- app/features/selectable.js
+++ app/features/selectable.js
@@ -73,7 +73,7 @@ export function Selectable() {
     let did_hide = false
 
     document.onkeydown = function(e) {
-      if (hotkeys.cmd && selected.length) {
+      if (hotkeys.ctrl && selected.length) {
         $('pb-handles, pb-label').forEach(el =>
           el.style.display = 'none')
         
