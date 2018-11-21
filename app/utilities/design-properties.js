5600bb3ca479788f38ff97da5feac32e8d207aab
Adam Argyle <atom@argyleink.com>
2018-11-21 13:10:03 -0800

    fix style inspector reporting styles that arent unique to the node

--- app/utilities/design-properties.js
+++ app/utilities/design-properties.js
@@ -25,6 +25,14 @@ export const desiredPropMap = {
   stroke:               'none',
 }
 
+if (navigator.userAgent.search('Firefox')) {
+  desiredPropMap.backgroundSize = 'auto auto'
+  desiredPropMap.borderWidth    = ''
+  desiredPropMap.borderRadius   = ''
+  desiredPropMap.padding        = ''
+  desiredPropMap.margin         = ''
+}
+
 export const desiredAccessibilityMap = [
   'role',
   'tabindex',
