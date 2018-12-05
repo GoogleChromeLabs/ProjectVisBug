460bf14702d731397ec93ed03753db9e17ad8a48
Adam Argyle <atom@argyleink.com>
2018-12-05 11:04:10 -0800

    groundwork for extracting metaKey per platform

--- app/utilities/index.js
+++ app/utilities/index.js
@@ -1,5 +1,5 @@
 export { getStyle, getStyles, getComputedBackgroundColor } from './styles'
 export { getA11ys, getWCAG2TextSize } from './accessibility'
 export { getSide, showHideSelected, showHideNodeLabel, htmlStringToDom, isOffBounds } from './common'
-export { camelToDash, nodeKey, createClassname } from './strings'
+export { camelToDash, nodeKey, createClassname, metaKey } from './strings'
 export { windowBounds } from './window'
