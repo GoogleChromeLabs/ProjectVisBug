c6477f9ebf7a9d0e8ee52e02abeca13a583f5be1
Adam Argyle <atom@argyleink.com>
2018-12-06 09:07:27 -0800

    fixes demo page showing cmd instead of ctrl in html

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
 import ToolPallete from './components/tool-pallete/toolpallete.element'
+import { metaKey } from './utilities';
+
+if (metaKey === 'ctrl')
+  [...document.querySelectorAll('kbd')]
+    .filter(node => node.textContent.includes('cmd'))
+    .forEach(node => node.textContent = node.textContent.replace('cmd','ctrl'))
