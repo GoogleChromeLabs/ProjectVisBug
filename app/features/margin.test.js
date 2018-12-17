be0cca63dd5e66162d2de1629810d2c10d400ffc
Adam Argyle <atom@argyleink.com>
2018-12-17 13:13:35 -0800

    almost have alt+up working and then we can fully test key combos

--- app/features/margin.test.js
+++ app/features/margin.test.js
@@ -6,6 +6,9 @@ const chrome = {}
 const marginMode = async () =>
   await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
 
+const getActiveTool = async () =>
+  await chrome.page.$eval('tool-pallete', el => el.activeTool)
+
 test.before(async () => {
   chrome.browser  = await puppeteer.launch()
   chrome.page     = await chrome.browser.newPage()
@@ -15,14 +18,14 @@ test.before(async () => {
 })
 
 test('Can Be Activated', async t => {
-  t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'margin')
+  t.is(await getActiveTool(), 'margin')
   t.pass()
 })
 
 test('Can Be Deactivated', async t => {
-  t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'margin')
+  t.is(await getActiveTool(), 'margin')
   await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
-  t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'padding')
+  t.is(await getActiveTool(), 'padding')
   
   t.pass()
 })
@@ -31,7 +34,25 @@ test('Adds margin to side', async t => {
   await chrome.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
   t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '')
   
-  await chrome.page.keyboard.down('ArrowUp')
+  await chrome.page.keyboard.press('ArrowUp')
+  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+
+  t.pass()
+})
+
+test('Remove margin from side', async t => {
+  await chrome.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '')
+  
+  await chrome.page.keyboard.press('ArrowUp')
+  await chrome.page.keyboard.press('ArrowUp')
+
+  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '2px')
+
+  await chrome.page.keyboard.down('Alt')
+  await chrome.page.keyboard.press('ArrowUp')
+  await chrome.page.keyboard.up('Alt')
+
   t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
 
   t.pass()
