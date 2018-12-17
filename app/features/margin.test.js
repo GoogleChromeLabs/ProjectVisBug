a3f02138e82ae41b5d337e0beba46e783ce05aba
Adam Argyle <atom@argyleink.com>
2018-12-17 12:41:18 -0800

    margin test now uses keyboard events and verifies element was modified

--- app/features/margin.test.js
+++ app/features/margin.test.js
@@ -3,19 +3,37 @@ import puppeteer from 'puppeteer'
 
 const chrome = {}
 
+const marginMode = async () =>
+  await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
+
 test.before(async () => {
   chrome.browser  = await puppeteer.launch()
   chrome.page     = await chrome.browser.newPage()
-  
+
   await chrome.page.goto('http://localhost:3000')
+  await marginMode()
 })
 
-test('Activated', async t => {
-  t.not(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'margin')
-
-  const marginBtn = await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
-  
+test('Can Be Activated', async t => {
   t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'margin')
+  t.pass()
+})
+
+test('Can Be Deactivated', async t => {
+  t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'margin')
+  await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
+  t.is(await chrome.page.$eval('tool-pallete', el => el.activeTool), 'padding')
+  
+  t.pass()
+})
+
+test('Adds margin to side', async t => {
+  await chrome.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '')
+  
+  await chrome.page.keyboard.down('ArrowUp')
+  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+
   t.pass()
 })
 
