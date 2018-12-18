28d17dd815709a353e3a174fabeec3be20728709
Adam Argyle <atom@argyleink.com>
2018-12-17 16:38:24 -0800

    concurrency issues resolves using test context to pass puppeteer objects

--- app/features/margin.test.js
+++ app/features/margin.test.js
@@ -1,64 +1,63 @@
 import test from 'ava'
 import puppeteer from 'puppeteer'
 
-const chrome = {}
+const marginMode = async p =>
+  await p.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
 
-const marginMode = async () =>
-  await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
+const getActiveTool = async p =>
+  await p.$eval('tool-pallete', el => el.activeTool)
 
-const getActiveTool = async () =>
-  await chrome.page.$eval('tool-pallete', el => el.activeTool)
+test.beforeEach(async t => {
+  t.context.browser  = await puppeteer.launch()
+  t.context.page     = await t.context.browser.newPage()
 
-test.before(async () => {
-  chrome.browser  = await puppeteer.launch()
-  chrome.page     = await chrome.browser.newPage()
-
-  await chrome.page.goto('http://localhost:3000')
-  await marginMode()
+  await t.context.page.goto('http://localhost:3000')
+  await marginMode(t.context.page)
 })
 
 test('Can Be Activated', async t => {
-  t.is(await getActiveTool(), 'margin')
+  t.is(await getActiveTool(t.context.page), 'margin')
   t.pass()
 })
 
 test('Can Be Deactivated', async t => {
-  t.is(await getActiveTool(), 'margin')
-  await chrome.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
-  t.is(await getActiveTool(), 'padding')
-  
+  t.is(await getActiveTool(t.context.page), 'margin')
+  await t.context.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
+  t.is(await getActiveTool(t.context.page), 'padding')
+
   t.pass()
 })
 
 test('Adds margin to side', async t => {
-  await chrome.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
-  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '')
-  
-  await chrome.page.keyboard.press('ArrowUp')
-  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+  await t.context.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '')
+
+  await t.context.page.keyboard.press('ArrowUp')
+
+  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
 
   t.pass()
 })
 
 test('Remove margin from side', async t => {
-  await chrome.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
-  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '')
-  
-  await chrome.page.keyboard.press('ArrowUp')
-  await chrome.page.keyboard.press('ArrowUp')
+  await t.context.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '')
 
-  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '2px')
+  await t.context.page.keyboard.press('ArrowUp', { delay: 10 })
+  await t.context.page.keyboard.press('ArrowUp', { delay: 10 })
 
-  await chrome.page.keyboard.down('Alt')
-  await chrome.page.keyboard.press('ArrowUp')
-  await chrome.page.keyboard.up('Alt')
+  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '2px')
 
-  t.is(await chrome.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+  await t.context.page.keyboard.down('AltLeft')
+  await t.context.page.keyboard.press('ArrowUp')
+  await t.context.page.keyboard.up('AltLeft')
+
+  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
 
   t.pass()
 })
 
-test.after.always(async () => {
-  await chrome.page.close()
-  await chrome.browser.close()
+test.afterEach(async t => {
+  await t.context.page.close()
+  await t.context.browser.close()
 })
