6896f98235ad86ce3502e703ffc1c69792827d62
Adam Argyle <atom@argyleink.com>
2018-12-17 20:02:15 -0800

    little code refactor

--- app/features/margin.test.js
+++ app/features/margin.test.js
@@ -1,11 +1,14 @@
 import test from 'ava'
 import puppeteer from 'puppeteer'
 
-const marginMode = async p =>
-  await p.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
+const marginMode = async page =>
+  await page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="margin"]').click()`)
 
-const getActiveTool = async p =>
-  await p.$eval('tool-pallete', el => el.activeTool)
+const getActiveTool = async page =>
+  await page.$eval('tool-pallete', el => el.activeTool)
+
+const getMarginTop = async page =>
+  await page.$eval('[intro] h2', el => el.style.marginTop)
 
 test.beforeEach(async t => {
   t.context.browser  = await puppeteer.launch()
@@ -21,43 +24,49 @@ test('Can Be Activated', async t => {
 })
 
 test('Can Be Deactivated', async t => {
-  t.is(await getActiveTool(t.context.page), 'margin')
-  await t.context.page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
-  t.is(await getActiveTool(t.context.page), 'padding')
+  const { page } = t.context
+
+  t.is(await getActiveTool(page), 'margin')
+  await page.evaluateHandle(`document.querySelector('tool-pallete').shadowRoot.querySelector('li[data-tool="padding"]').click()`)
+  t.is(await getActiveTool(page), 'padding')
 
   t.pass()
 })
 
 test('Adds margin to side', async t => {
-  await t.context.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
-  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '')
+  const { page } = t.context
 
-  await t.context.page.keyboard.press('ArrowUp')
+  await page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await getMarginTop(page), '')
 
-  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+  await page.keyboard.press('ArrowUp')
+
+  t.is(await getMarginTop(page), '1px')
 
   t.pass()
 })
 
 test('Remove margin from side', async t => {
-  await t.context.page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
-  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '')
+  const { page } = t.context
 
-  await t.context.page.keyboard.press('ArrowUp', { delay: 10 })
-  await t.context.page.keyboard.press('ArrowUp', { delay: 10 })
+  await page.evaluateHandle(`document.querySelector('[intro] h2').click()`)
+  t.is(await getMarginTop(page), '')
 
-  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '2px')
+  await page.keyboard.press('ArrowUp', { delay: 10 })
+  await page.keyboard.press('ArrowUp', { delay: 10 })
 
-  await t.context.page.keyboard.down('AltLeft')
-  await t.context.page.keyboard.press('ArrowUp')
-  await t.context.page.keyboard.up('AltLeft')
+  t.is(await getMarginTop(page), '2px')
 
-  t.is(await t.context.page.$eval('[intro] h2', el => el.style.marginTop), '1px')
+  await page.keyboard.down('Alt')
+  await page.keyboard.press('ArrowUp')
+  await page.keyboard.up('Alt')
+
+  t.is(await getMarginTop(page), '1px')
 
   t.pass()
 })
 
-test.afterEach(async t => {
-  await t.context.page.close()
-  await t.context.browser.close()
+test.afterEach(async ({context:{ page, browser }}) => {
+  await page.close()
+  await browser.close()
 })
