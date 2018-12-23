3c6d7e680cb44af9082cfe81270e8ffad70dc1be
Adam Argyle <atom@argyleink.com>
2018-12-23 02:49:15 -0800

    runner to manage server and tests

---
+++ tests/runner.js
@@ -0,0 +1,10 @@
+const { spawn, exec } = require('child_process')
+
+const server  = spawn('npm run dev:server', {shell:true})
+
+exec('npm test', (error, stdout, stderr) => 
+  error
+    ? server.kill(error)
+    : console.log(stdout))
+
+server.kill()
