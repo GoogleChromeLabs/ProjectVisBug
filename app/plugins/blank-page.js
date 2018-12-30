0c13e3a7c84c84292bdf1ff3158c0bff1bcde83a
Adam Argyle <atom@argyleink.com>
2018-12-30 14:15:03 -0800

    ok to remove gridlines

--- app/plugins/blank-page.js
+++ app/plugins/blank-page.js
@@ -1,5 +1,5 @@
 export default function() {
   document
-    .querySelectorAll('body > *:not(pb-gridlines):not(script):not(tool-pallete)')
+    .querySelectorAll('body > *:not(tool-pallete):not(script)')
     .forEach(node => node.remove())
 }
