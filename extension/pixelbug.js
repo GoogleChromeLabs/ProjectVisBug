2e0e31158f24c1f0b4528cbca69ec492994bec43
Adam Argyle <atom@argyleink.com>
2019-01-17 21:24:45 -0800

    context menu invocation

--- extension/pixelbug.js
+++ extension/pixelbug.js
@@ -1,29 +1,51 @@
-let state = {
+const state = {
   loaded:   {},
   injected: {},
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (state.loaded[tab.id] && state.injected[tab.id]) {
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/eject.js' })
-    state.injected[tab.id] = false
+const toggleIn = ({id:tab_id}) => {
+  // toggle out: it's currently loaded and injected
+  if (state.loaded[tab_id] && state.injected[tab_id]) {
+    chrome.tabs.executeScript(tab_id, { file: 'toolbar/eject.js' })
+    state.injected[tab_id] = false
   }
-  else if (state.loaded[tab.id] && !state.injected[tab.id]) {
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
-    state.injected[tab.id] = true
-  }
-  else {
-    chrome.tabs.insertCSS(tab.id,     { file: 'toolbar/bundle.css' })
-    chrome.tabs.executeScript(tab.id, { file: 'web-components.polyfill.js' })
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/bundle.js' })
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
 
-    state.loaded[tab.id]    = true
-    state.injected[tab.id]  = true
+  // toggle in: it's loaded and needs injected
+  else if (state.loaded[tab_id] && !state.injected[tab_id]) {
+    chrome.tabs.executeScript(tab_id, { file: 'toolbar/content.js' })
+    state.injected[tab_id] = true
+  }
+
+  // fresh start in tab
+  else {
+    chrome.tabs.insertCSS(tab_id,     { file: 'toolbar/bundle.css' })
+    chrome.tabs.executeScript(tab_id, { file: 'web-components.polyfill.js' })
+    chrome.tabs.executeScript(tab_id, { file: 'toolbar/bundle.js' })
+    chrome.tabs.executeScript(tab_id, { file: 'toolbar/content.js' })
+
+    state.loaded[tab_id]    = true
+    state.injected[tab_id]  = true
   }
 
   chrome.tabs.onUpdated.addListener(function(tabId) {
-    if (tabId === tab.id)
+    if (tabId === tab_id)
       state.loaded[tabId] = false
+  })
+}
+
+chrome.browserAction.onClicked.addListener(toggleIn)
+
+chrome.contextMenus.create({
+  title: 'Inspect'
+})
+
+chrome.contextMenus.onClicked.addListener((menuInfo, tab) => {
+  toggleIn(tab)
+
+  chrome.tabs.query({active: true, currentWindow: true}, function([tab]) {
+    tab && chrome.tabs.sendMessage(tab.id, {
+      action: 'toolSelected',
+      params: 'inspector',
+    })
   })
 })
