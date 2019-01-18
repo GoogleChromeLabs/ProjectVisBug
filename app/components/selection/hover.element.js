b00ef064e0e826443130d05ded152bd9a5c70597
Adam Argyle <atom@argyleink.com>
2019-01-18 08:27:23 -0800

    hover stroke width is slightly thinner to be less visually dominant

--- app/components/selection/hover.element.js
+++ app/components/selection/hover.element.js
@@ -2,7 +2,7 @@ import $ from 'blingblingjs'
 import { Handles } from './handles.element'
 
 export class Hovers extends Handles {
-  
+
   constructor() {
     super()
   }
@@ -11,13 +11,13 @@ export class Hovers extends Handles {
     this.$shadow.host.setAttribute('data-label-id', node_label_id)
     return `
       ${this.styles({top,left})}
-      <svg 
+      <svg
         class="pb-hovers"
-        width="${width}" height="${height}" 
-        viewBox="0 0 ${width} ${height}" 
+        width="${width}" height="${height}"
+        viewBox="0 0 ${width} ${height}"
         version="1.1" xmlns="http://www.w3.org/2000/svg"
       >
-        <rect stroke="hsl(267, 100%, 58%)" fill="none" width="100%" height="100%"></rect>
+        <rect stroke-width="0.5px" stroke="hsl(267, 100%, 58%)" fill="none" width="100%" height="100%"></rect>
       </svg>
     `
   }
