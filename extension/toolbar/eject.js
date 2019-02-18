cec5a137a19f77e6368c9c229ff6dd57cab0c726
Adam Argyle <atom@argyleink.com>
2019-02-17 20:06:16 -0800

    remove any lingering labels too

--- extension/toolbar/eject.js
+++ extension/toolbar/eject.js
@@ -1,4 +1,5 @@
 [
   ...document.getElementsByTagName('vis-bug'),
   ...document.getElementsByTagName('visbug-hover'),
+  ...document.getElementsByTagName('visbug-label'),
 ].forEach(el => el.remove())
