37d6f85d3068a4f0653310067ef0dea37afd49f2
Adam Argyle <atom@argyleink.com>
2019-03-07 22:30:43 -0800

    nearly fixes #145

--- app/features/margin.js
+++ app/features/margin.js
@@ -1,7 +1,6 @@
 import hotkeys from 'hotkeys-js'
 import { metaKey, getStyle, getSide, showHideSelected } from '../utilities/'
 
-// todo: show margin color
 const key_events = 'up,down,left,right'
   .split(',')
   .reduce((events, event) =>
@@ -11,23 +10,26 @@ const key_events = 'up,down,left,right'
 
 const command_events = `${metaKey}+up,${metaKey}+shift+up,${metaKey}+down,${metaKey}+shift+down`
 
-export function Margin({selection}) {
+export function Margin(visbug) {
   hotkeys(key_events, (e, handler) => {
     if (e.cancelBubble) return
 
     e.preventDefault()
-    pushElement(selection(), handler.key)
+    pushElement(visbug.selection(), handler.key)
   })
 
   hotkeys(command_events, (e, handler) => {
     e.preventDefault()
-    pushAllElementSides(selection(), handler.key)
+    pushAllElementSides(visbug.selection(), handler.key)
   })
+
+  visbug.onSelectedUpdate(paintBackground)
 
   return () => {
     hotkeys.unbind(key_events)
     hotkeys.unbind(command_events)
     hotkeys.unbind('up,down,left,right') // bug in lib?
+    visbug.removeSelectedCallback(paintBackground)
   }
 }
 
@@ -60,4 +62,70 @@ export function pushAllElementSides(els, keycommand) {
 
   'up,down,left,right'.split(',')
     .forEach(side => pushElement(els, spoof + side))
+}
+
+function paintBackground(els) {
+  els.forEach(el => {
+    const label_id          = el.getAttribute('data-label-id')
+    const bounds            = el.getBoundingClientRect()
+    const styleOM           = el.computedStyleMap()
+    const calculatedStyle   = getStyle(el, 'margin')
+
+    const margin = {
+      top:    styleOM.get('margin-top').value,
+      right:  styleOM.get('margin-right').value,
+      bottom: styleOM.get('margin-bottom').value,
+      left:   styleOM.get('margin-left').value,
+    }
+
+    const $el_handle = document
+      .querySelector(`visbug-handles[data-label-id="${label_id}"]`)
+      .$shadow
+
+    const handle_style = $el_handle.querySelector('svg').style
+
+    
+    if (calculatedStyle === '0px')
+      return
+
+    const highlight = document.createElement('div')
+
+    highlight.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+        <defs>
+          <pattern id="pinstripe" patternUnits="userSpaceOnUse" width="10" height="10" patternTransform="rotate(-45)" class="pattern">
+            <line x1="0" y="0" x2="0" y2="10" stroke="hotpink" stroke-width="1"></line>
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#pinstripe)"></rect>
+      </svg>
+    `
+
+    const total_height = bounds.height + margin.bottom + margin.top
+    const total_width = bounds.width + margin.right + margin.left
+
+    highlight.style = `
+      position: absolute;
+      z-index: 1;
+      width: ${total_width}px;
+      height: ${total_height}px;
+      top: ${bounds.top + window.scrollY - margin.top}px;
+      left: ${bounds.left - margin.left}px;
+      background-color: hsla(330, 100%, 71%, 8%);
+      clip-path: polygon(
+        0% 0%, 0% 100%, ${margin.left}px 100%, 
+        ${margin.left}px ${margin.top}px, 
+        ${total_width - margin.right}px ${margin.top}px, 
+        ${total_width - margin.right}px ${total_height - margin.bottom}px, 
+        0 ${total_height - margin.bottom}px, 0 100%, 
+        100% 100%, 100% 0%
+      );
+    `
+
+    const has_child = $el_handle.querySelector('div')
+
+    has_child
+      ? $el_handle.replaceChild(highlight, has_child)
+      : $el_handle.appendChild(highlight)
+  })
 }
--- app/features/padding.js
+++ app/features/padding.js
@@ -1,7 +1,6 @@
 import hotkeys from 'hotkeys-js'
 import { metaKey, getStyle, getSide, showHideSelected } from '../utilities/'
 
-// todo: show padding color
 const key_events = 'up,down,left,right'
   .split(',')
   .reduce((events, event) =>
@@ -11,23 +10,26 @@ const key_events = 'up,down,left,right'
 
 const command_events = `${metaKey}+up,${metaKey}+shift+up,${metaKey}+down,${metaKey}+shift+down`
 
-export function Padding({selection}) {
+export function Padding(visbug) {
   hotkeys(key_events, (e, handler) => {
     if (e.cancelBubble) return
 
     e.preventDefault()
-    padElement(selection(), handler.key)
+    padElement(visbug.selection(), handler.key)
   })
 
   hotkeys(command_events, (e, handler) => {
     e.preventDefault()
-    padAllElementSides(selection(), handler.key)
+    padAllElementSides(visbug.selection(), handler.key)
   })
+
+  visbug.onSelectedUpdate(paintBackground)
 
   return () => {
     hotkeys.unbind(key_events)
     hotkeys.unbind(command_events)
     hotkeys.unbind('up,down,left,right') // bug in lib?
+    visbug.removeSelectedCallback(paintBackground)
   }
 }
 
@@ -60,4 +62,66 @@ export function padAllElementSides(els, keycommand) {
 
   'up,down,left,right'.split(',')
     .forEach(side => padElement(els, spoof + side))
+}
+
+function paintBackground(els) {
+  els.forEach(el => {
+    const label_id          = el.getAttribute('data-label-id')
+    const bounds            = el.getBoundingClientRect()
+    const styleOM           = el.computedStyleMap()
+    const calculatedStyle   = getStyle(el, 'padding')
+
+    const padding = {
+      top:    styleOM.get('padding-top').value,
+      right:  styleOM.get('padding-right').value,
+      bottom: styleOM.get('padding-bottom').value,
+      left:   styleOM.get('padding-left').value,
+    }
+
+    const $el_handle = document
+      .querySelector(`visbug-handles[data-label-id="${label_id}"]`)
+      .$shadow
+
+    const handle_style = $el_handle.querySelector('svg').style
+    
+    if (calculatedStyle === '0px')
+      return
+
+    const highlight = document.createElement('div')
+
+    highlight.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+        <defs>
+          <pattern id="pinstripe" patternUnits="userSpaceOnUse" width="10" height="10" patternTransform="rotate(-45)" class="pattern">
+            <line x1="0" y="0" x2="0" y2="10" stroke="hotpink" stroke-width="1"></line>
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#pinstripe)"></rect>
+      </svg>
+    `
+
+    highlight.style = `
+      position: absolute;
+      z-index: 1;
+      width: ${bounds.width}px;
+      height: ${bounds.height}px;
+      top: ${bounds.top + window.scrollY}px;
+      left: ${bounds.left}px;
+      background-color: hsla(330, 100%, 71%, 8%);
+      clip-path: polygon(
+        0% 0%, 0% 100%, ${padding.left}px 100%, 
+        ${padding.left}px ${padding.top}px, 
+        ${bounds.width - padding.right}px ${padding.top}px, 
+        ${bounds.width - padding.right}px ${bounds.height - padding.bottom}px, 
+        0 ${bounds.height - padding.bottom}px, 0 100%, 
+        100% 100%, 100% 0%
+      );
+    `
+
+    const has_child = $el_handle.querySelector('div')
+
+    has_child
+      ? $el_handle.replaceChild(highlight, has_child)
+      : $el_handle.appendChild(highlight)
+  })
 }
