34920e98cf4aeef1e1f777954ddddf8f68cc55a4
Adam Argyle <atom@argyleink.com>
2019-03-10 23:48:52 -0700

    box model draws margin sizing

--- app/components/selection/box-model.element.js
+++ app/components/selection/box-model.element.js
@@ -3,23 +3,24 @@ export class BoxModel extends HTMLElement {
   constructor() {
     super()
     this.$shadow = this.attachShadow({mode: 'closed'})
+    this.drawable = {}
   }
 
   connectedCallback() {}
   disconnectedCallback() {}
 
   set position(payload) {
-    this.$shadow.innerHTML  = this.render(payload)
+    this.$shadow.innerHTML = this.render(payload)
+    if (!this.drawable.measurements) // && payload.color === 'pink'
+      this.createMeasurements(payload)
   }
 
   render({mode, bounds, sides, color = 'pink'}) {
     const total_height  = bounds.height + sides.bottom + sides.top
     const total_width   = bounds.width + sides.right + sides.left
 
-    let drawable = {}
-
     if (mode === 'padding') {
-      drawable = {
+      this.drawable = {
         height:   bounds.height,
         width:    bounds.width,
         top:      bounds.top + window.scrollY,
@@ -28,7 +29,7 @@ export class BoxModel extends HTMLElement {
       }
     }
     else if (mode === 'margin') {
-      drawable = {
+      this.drawable = {
         height:   total_height,
         width:    total_width,
         top:      bounds.top + window.scrollY - sides.top,
@@ -38,12 +39,12 @@ export class BoxModel extends HTMLElement {
     }
 
     if (color === 'pink') {
-      drawable.bg = 'hsla(330, 100%, 71%, 15%)'
-      drawable.stripe = 'hsla(330, 100%, 71%, 80%)'
+      this.drawable.bg = 'hsla(330, 100%, 71%, 15%)'
+      this.drawable.stripe = 'hsla(330, 100%, 71%, 80%)'
     }
     else {
-      drawable.bg = 'hsla(267, 100%, 58%, 15%)'
-      drawable.stripe = 'hsla(267, 100%, 58%, 80%)'
+      this.drawable.bg = 'hsla(267, 100%, 58%, 15%)'
+      this.drawable.stripe = 'hsla(267, 100%, 58%, 80%)'
     }
 
     return `
@@ -51,30 +52,81 @@ export class BoxModel extends HTMLElement {
         pointer-events: none;
         position: absolute;
         z-index: 2147483642;
-        width: ${drawable.width}px;
-        height: ${drawable.height}px;
-        top: ${drawable.top}px;
-        left: ${drawable.left}px;
-        background-color: ${drawable.bg};
+        width: ${this.drawable.width}px;
+        height: ${this.drawable.height}px;
+        top: ${this.drawable.top}px;
+        left: ${this.drawable.left}px;
+        background-color: ${this.drawable.bg};
         clip-path: polygon(
           0% 0%, 0% 100%, ${sides.left}px 100%, 
           ${sides.left}px ${sides.top}px, 
-          ${drawable.width - sides.right}px ${sides.top}px, 
-          ${drawable.width - sides.right}px ${drawable.height - sides.bottom}px, 
-          0 ${drawable.height - sides.bottom}px, 0 100%, 
+          ${this.drawable.width - sides.right}px ${sides.top}px, 
+          ${this.drawable.width - sides.right}px ${this.drawable.height - sides.bottom}px, 
+          0 ${this.drawable.height - sides.bottom}px, 0 100%, 
           100% 100%, 100% 0%
         );
       ">
         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
           <defs>
-            <pattern id="pinstripe" patternUnits="userSpaceOnUse" width="10" height="10" patternTransform="${drawable.rotation}" class="pattern">
-              <line x1="0" y="0" x2="0" y2="10" stroke="${drawable.stripe}" stroke-width="1"></line>
+            <pattern id="pinstripe" patternUnits="userSpaceOnUse" width="10" height="10" patternTransform="${this.drawable.rotation}" class="pattern">
+              <line x1="0" y="0" x2="0" y2="10" stroke="${this.drawable.stripe}" stroke-width="1"></line>
             </pattern>
           </defs>
           <rect width="100%" height="100%" fill="url(#pinstripe)"></rect>
         </svg>
       </div>
     `
+  }
+
+  createMeasurements({mode, bounds, sides, color}) {
+    if (mode === 'margin') {
+      if (sides.top) {
+        this.createMeasurement({
+          x: bounds.left + (bounds.width / 2) - 3,
+          y: bounds.top - sides.top,
+          d: sides.top,
+          q: 'top',
+          v: true,
+          color,
+        })
+      }
+      if (sides.bottom) {
+        this.createMeasurement({
+          x: bounds.left + (bounds.width / 2) - 3,
+          y: bounds.bottom,
+          d: sides.bottom,
+          q: 'bottom',
+          v: true,
+          color,
+        })
+      }
+      if (sides.right) {
+        this.createMeasurement({
+          x: bounds.right,
+          y: bounds.top + (bounds.height / 2) - 3,
+          d: sides.right,
+          q: 'right',
+          v: false,
+          color,
+        })
+      }
+      if (sides.left) {
+        this.createMeasurement({
+          x: window.innerWidth - bounds.left,
+          y: bounds.top + (bounds.height / 2) - 3,
+          d: sides.left,
+          q: 'left',
+          v: false,
+          color,
+        })
+      }
+    }
+  }
+
+  createMeasurement(line_model, node_label_id=0) {
+    const measurement = document.createElement('visbug-distance')
+    measurement.position = { line_model, node_label_id }
+    this.$shadow.appendChild(measurement)
   }
 }
 
