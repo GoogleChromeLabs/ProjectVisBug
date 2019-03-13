64727b7bf5a873084c8a50b6ad624b8c7c058259
Adam Argyle <atom@argyleink.com>
2019-03-13 16:14:47 -0700

    almost finished with the distance element logic

--- app/components/selection/distance.element.js
+++ app/components/selection/distance.element.js
@@ -5,7 +5,7 @@ export class Distance extends HTMLElement {
   constructor() {
     super()
     this.$shadow = this.attachShadow({mode: 'open'})
-    // this.$shadow.adoptedStyleSheets = [DistanceStyles]
+    this.$shadow.adoptedStyleSheets = [DistanceStyles]
   }
 
   connectedCallback() {}
@@ -22,22 +22,22 @@ export class Distance extends HTMLElement {
 
     this.style.setProperty('--top', `${y + window.scrollY}px`)
     this.style.setProperty('--left', `${x}px`)
-    this.style.setProperty('direction', v ? 'column' : 'row')
+    this.style.setProperty('--direction', v ? 'column' : 'row')
+    this.style.setProperty('--quadrant', q)
 
     v
       ? this.style.setProperty('--distance-h', `${d}px`)
       : this.style.setProperty('--distance-w', `${d}px`)
 
      v
-      ? this.style.setProperty('--line-h', `var(--line-width)`)
-      : this.style.setProperty('--line-w', `var(--line-width)`)
+      ? this.style.setProperty('--line-h', `var(--line-w)`)
+      : this.style.setProperty('--line-w', `var(--line-w)`)
 
     this.style.setProperty('--translate', v
       ? 'translateX(-50%)'
       : 'translateY(-50%)')
 
     return `
-      ${this.styles(measurements)}
       <figure quadrant="measurements.q">
         <div></div>
         <figcaption><b>${measurements.d}</b>px</figcaption>
