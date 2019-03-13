e564c7d4d15a147b4957890a405116aeb1bc0d61
Adam Argyle <atom@argyleink.com>
2019-03-13 07:58:28 -0700

    fixed tota11y loading issue

--- app/plugins/tota11y.js
+++ app/plugins/tota11y.js
@@ -3,5 +3,5 @@ export const commands = [
 ]
 
 export default async function() {
-  await import('//cdnjs.cloudflare.com/ajax/libs/tota11y/0.1.6/tota11y.min.js')
+  await import('https://cdnjs.cloudflare.com/ajax/libs/tota11y/0.1.6/tota11y.min.js')
 }
