4899db0ba48689a6a9a383dfb17daccd2e88f16f
Adam Argyle <atom@argyleink.com>
2019-03-14 00:19:08 -0700

    added gridlines stylesheet

--- app/components/styles.store.js
+++ app/components/styles.store.js
@@ -1,6 +1,7 @@
-import { default as handle_css }   from './selection/handles.element.css'
-import { default as hover_css }    from './selection/hover.element.css'
-import { default as distance_css } from './selection/distance.element.css'
+import { default as handle_css }    from './selection/handles.element.css'
+import { default as hover_css }     from './selection/hover.element.css'
+import { default as distance_css }  from './selection/distance.element.css'
+import { default as gridline_css } from './selection/gridlines.element.css'
 
 const constructStylesheet = (styles, stylesheet = new CSSStyleSheet()) => {
   stylesheet.replaceSync(styles)
@@ -10,9 +11,11 @@ const constructStylesheet = (styles, stylesheet = new CSSStyleSheet()) => {
 const HandleStyles   = constructStylesheet(handle_css)
 const HoverStyles    = constructStylesheet(hover_css)
 const DistanceStyles = constructStylesheet(distance_css)
+const GridlineStyles = constructStylesheet(gridline_css)
 
 export {
   HandleStyles,
   HoverStyles,
   DistanceStyles,
+  GridlineStyles
 }
