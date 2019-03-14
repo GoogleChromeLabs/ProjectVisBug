84d2f5bb80a10ec07a407bea440ecdaf328bb200
Adam Argyle <atom@argyleink.com>
2019-03-14 10:39:58 -0700

    initial port of code

--- app/components/selection/overlay.element.js
+++ app/components/selection/overlay.element.js
@@ -1,51 +1,40 @@
+import { OverlayStyles } from '../styles.store'
+
 export class Overlay extends HTMLElement {
-  
+
   constructor() {
     super()
     this.$shadow = this.attachShadow({mode: 'closed'})
+    this.$shadow.adoptedStyleSheets = [OverlayStyles]
   }
 
   connectedCallback() {}
   disconnectedCallback() {}
 
   set position(boundingRect) {
-    this.$shadow.innerHTML  = this.render(boundingRect)
+    this.$shadow.innerHTML = this.render(boundingRect)
   }
 
   set update({ top, left, width, height }) {
-    this.$shadow.host.style.display = 'block'
+    const [svg] = this.$shadow.children
 
-    const svg = this.$shadow.children[0]
+    this.$shadow.host.style.display = 'block'
     svg.style.display = 'block'
-    svg.style.top = window.scrollY + top + 'px'
-    svg.style.left = left + 'px'
+
+    this.style.setProperty('--top', `${top + window.scrollY}px`)
+    this.style.setProperty('--left', `${left - 1}px`)
 
     svg.setAttribute('width', width + 'px')
     svg.setAttribute('height', height + 'px')
   }
 
-  render({id, top, left, height, width}) {
+  render({height, width}) {
     return `
-      <svg 
-        class="visbug-overlay"
-        overlay-id="${id}"
-        style="
-          display:none;
-          position:absolute;
-          top:0;
-          left:0;
-          overflow:visible;
-          pointer-events:none;
-          z-index: 999;
-        " 
-        width="${width}px" height="${height}px" 
-        viewBox="0 0 ${width} ${height}" 
-        version="1.1" xmlns="http://www.w3.org/2000/svg"
+      <svg class="visbug-overlay"
+        width="${width}px" height="${height}px"
+        viewBox="0 0 ${width} ${height}"
       >
-        <rect 
-          fill="hsla(330, 100%, 71%, 0.5)"
-          width="100%" height="100%"
-        ></rect>
+        <rect></rect>
       </svg>
     `
   }
---
+++ app/components/styles.store.js
@@ -0,0 +1,27 @@
+import { default as handle_css }   from './selection/handles.element.css'
+import { default as hover_css }    from './selection/hover.element.css'
+import { default as distance_css } from './selection/distance.element.css'
+import { default as gridline_css } from './selection/gridlines.element.css'
+import { default as label_css }    from './selection/label.element.css'
+import { default as overlay_css }  from './selection/overlay.element.css'
+
+const constructStylesheet = (styles, stylesheet = new CSSStyleSheet()) => {
+  stylesheet.replaceSync(styles)
+  return stylesheet
+}
+
+const HandleStyles   = constructStylesheet(handle_css)
+const HoverStyles    = constructStylesheet(hover_css)
+const DistanceStyles = constructStylesheet(distance_css)
+const GridlineStyles = constructStylesheet(gridline_css)
+const LabelStyles    = constructStylesheet(label_css)
+const OverlayStyles  = constructStylesheet(overlay_css)
+
+export {
+  HandleStyles,
+  HoverStyles,
+  DistanceStyles,
+  GridlineStyles,
+  LabelStyles,
+  OverlayStyles,
+}
