0f3e2fdbd95d2b42415fd3d3b5d1750b380d57b1
Adam Argyle <atom@argyleink.com>
2019-04-18 21:41:24 -0700

    pass no sandbox arg

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer'
 
-const setupPptrTab = async t => {
-  t.context.browser  = await puppeteer.launch()
+export const setupPptrTab = async t => {
+  t.context.browser  = await puppeteer.launch({args: ['--no-sandbox']})
   t.context.page     = await t.context.browser.newPage()
 
   await t.context.page.goto('http://localhost:3000')
