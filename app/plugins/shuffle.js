8360f2eeb4240c6a0f85bfcd7bfe055521b398f5
fxnoob <fxnoob71@gmail.com>
2019-05-18 21:10:59 +0530

    shuffle plugin

---
+++ app/plugins/shuffle.js
@@ -0,0 +1,49 @@
+export const commands = [
+  'shuffle',
+]
+
+export default async (selectedElement) => {
+  const getSiblings =  (elem) => {
+    // Setup siblings array and get the first sibling
+    let siblings = [];
+    let sibling = elem.firstChild;
+    // Loop through each sibling and push to the array
+    while (sibling) {
+      if (sibling.nodeType === 1 && sibling !== elem) {
+        siblings.push(sibling);
+      }
+      sibling = sibling.nextSibling
+    }
+    return siblings;
+  };
+  const shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };
+  const appendSuffledSiblings = (element, suffledElementsArray) => {
+    element.innerHTML = '';
+    for (let i = 0; i < suffledElementsArray.length; i++) {
+      element.appendChild(suffledElementsArray[i])
+    }
+  };
+  const elem = selectedElement;
+  console.log("elem", selectedElement)
+  const siblings = getSiblings(elem);
+  console.log(siblings)
+  const shuffledSiblings = shuffle(siblings);
+  console.log(shuffledSiblings)
+  appendSuffledSiblings(elem, shuffledSiblings);
+}
