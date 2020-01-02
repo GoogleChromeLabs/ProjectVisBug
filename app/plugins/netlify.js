cb6721b15b8b0b36535df7ff0decc5fa9e28ab5a
Adam Argyle <argyle@google.com>
2020-01-02 09:50:25 -0800

    netlify plugin prep

---
+++ app/plugins/netlify.js
@@ -0,0 +1,9 @@
+export const commands = ['netlify']
+
+export default async function({selected, query}) {
+  debugger
+  // convert DOM to a stringified version
+  // post to netlify
+  // show indeterminate upload UI (nice to have)
+  // success: open new tab to new netlify page
+}
