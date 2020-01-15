cc16318433fbc39cd3826446f21130123b7b51db
Adam Argyle <argyle@google.com>
2020-01-15 15:16:15 -0800

    new trick, inject on page not from extension

--- extension/toolbar/inject.js
+++ extension/toolbar/inject.js
@@ -1,5 +1,9 @@
-var visbug = document.createElement('vis-bug')
-var src_path = chrome.extension.getURL(`tuts/guides.gif`)
+const script = document.createElement('script')
+script.src = chrome.runtime.getURL('toolbar/bundle.min.js')
+document.body.appendChild(script)
+
+const visbug = document.createElement('vis-bug')
+const src_path = chrome.runtime.getURL(`tuts/guides.gif`)
 
 visbug.tutsBaseURL = src_path.slice(0, src_path.lastIndexOf('/'))
 
