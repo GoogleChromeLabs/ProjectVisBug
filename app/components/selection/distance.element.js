b5a915e83cbfd4dac64fc6ba5d6dd55a7be0d466
Adam Argyle <argyle@google.com>
2020-06-17 15:27:02 -0700

    fixes left quadrant measurement regression

--- app/components/selection/distance.element.js
+++ app/components/selection/distance.element.js
@@ -20,7 +20,7 @@ export class Distance extends HTMLElement {
 
   set styleProps({y,x,d,q,v = false, color}) {
     this.style.setProperty('--top', `${y + window.scrollY}px`)
-    this.style.setProperty('--right', q === 'left' ? `${x+window.scrollX}px` : 'auto')
+    this.style.setProperty('--right', q === 'left' ? `${x-window.scrollX}px` : 'auto')
     this.style.setProperty('--left', q !== 'left' ? `${x+window.scrollX}px` : 'auto')
     this.style.setProperty('--direction', v ? 'column' : 'row')
     this.style.setProperty('--quadrant', q)
