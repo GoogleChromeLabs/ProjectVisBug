9c4ada205db28e90b1b7cd82f198c537dc5fe817
Adam Argyle <argyle@google.com>
2020-11-02 14:11:00 -0800

    add safari project

---
+++ VisBug/VisBug/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  VisBug
+//
+//  Created by Adam Argyle on 11/2/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
