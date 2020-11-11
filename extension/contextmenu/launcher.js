fb6b5066349758041574cce238c451ff5e7da0fb
Adam Argyle <argyle@google.com>
2020-11-11 12:02:44 -0800

    updates context menu show/hide logic to cross platform

--- extension/contextmenu/launcher.js
+++ extension/contextmenu/launcher.js
@@ -1,12 +1,16 @@
-chrome.browserAction.onClicked.addListener(toggleIn)
+var platform = typeof browser === 'undefined'
+  ? chrome
+  : browser
 
-chrome.contextMenus.create({
+platform.browserAction.onClicked.addListener(toggleIn)
+
+platform.contextMenus.create({
   id:     'launcher',
   title:  'Show/Hide',
   contexts: ['all'],
 })
 
-chrome.contextMenus.onClicked.addListener(({menuItemId}, tab) => {
+platform.contextMenus.onClicked.addListener(({menuItemId}, tab) => {
   if (menuItemId === 'launcher')
     toggleIn(tab)
 })
