2268300319605aa79e9dc4b9b063bf9c335e26f1
Adam Argyle <argyle@google.com>
2020-12-01 11:20:33 -0800

    Update app/plugins/zindex.js

--- app/plugins/zindex.js
+++ app/plugins/zindex.js
@@ -13,7 +13,7 @@ export default function () {
         .filter(el => el.computedStyleMap().get('z-index').value !== 'auto')
         .filter(el => el.nodeName !== 'VIS-BUG')
         .forEach(el => {
-            const colors = ["#eb4034", "#68eb34", "#34a5eb", "#4334eb", "#b134eb", "#eb34a2", "#eb3446", "#8f2e2b", , "#8f692b", "#8a8f2b", "#358f2b", "#2b8f82", "#2b678f", "#2b2b8f", "#8f2b8f", "#8f2b55", "#1eff00", "#ff9d00", "#ff0000", "#00ff6a", "#0026ff", "#bb00ff", "#ff00d9", "#ff0073", "#ff0073", "#137878"];
+            const colors = ["#eb4034", "#30850f", "#116da7", "#4334eb", "#b134eb", "#df168e", "#e8172c", "#8f2e2b", "#8f692b", "#8a8f2b", "#358f2b", "#2b8f82", "#2b678f", "#2b2b8f", "#8f2b8f", "#8f2b55", "#1eff00", "#a86800", "#ff0000", "#008035", "#0026ff", "#bb00ff", "#d600b6", "#e60067", "#137878"];
             const color = colors[numberBetween(0, colors.length)];
             const zindex = el.computedStyleMap().get('z-index').value
 
