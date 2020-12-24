e36b96dad1881d67c3332202855415d54eb3af76
aashu0148 <buildforfb@gmail.com>
2020-12-24 11:15:38 +0530

    Issue #480

--- app/plugins/download-images.js
+++ app/plugins/download-images.js
@@ -3,22 +3,55 @@ export const commands = [
   'download-all-images',
 ]
 
-export default async function() {
+export default async function () {
+  if (window.showDirectoryPicker === undefined) {
+    console.log('no directory support :(')
+    return;
+  }
   const dirHandle = await window.showDirectoryPicker()
   const imgs = document.querySelectorAll("img")
-  let i = 0
 
   imgs.forEach(async (img) => {
     const url = img.src
-    const name = `img-${i}.png`
-    i++
-
+    const name = url.substr(url.lastIndexOf('/') + 1);
     try {
-      console.log(`Fetching ${url}`)
       const response = await fetch(url)
+      const file = await dirHandle.getFileHandle(name, { create: true })
+      const writable = await file.createWritable()
+      await response.body.pipeTo(writable)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+  // CSS urls
+  let css_urls = [...document.styleSheets]
+    .filter(sheet => {
+      try { return sheet.cssRules }
+      catch { }
+    })
+    .flatMap(sheet => Array.from(sheet.cssRules))
+    .filter(rule => rule.style)
+    .filter(rule => rule.style.backgroundImage !== '')
+    .filter(rule => rule.style.backgroundImage !== 'initial')
+    .filter(rule => rule.style.backgroundImage.includes("url"))
+    .reduce((urls, { style }) => {
+      urls.push(style.backgroundImage);
+      return urls;
+    }, []);
 
-      console.log(`Saving to ${name}`)
-
+  css_urls.forEach(async (item, i) => {
+    let cots = false;
+    let start = item.indexOf('(');
+    if (item.charAt(start + 1) == '"') cots = true;
+    let end = item.lastIndexOf(')');
+    if (cots) {
+      start += 2;
+      end -= 6;
+    }
+    const url = item.substr(start, end);
+    const name = `css-${i + 1}.jpg`;
+    try {
+      const response = await fetch(url)
       const file = await dirHandle.getFileHandle(name, { create: true })
       const writable = await file.createWritable()
       await response.body.pipeTo(writable)
