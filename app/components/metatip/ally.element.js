43cc4826a956075fdedc045f5b22ce67c66dc148
hchiam <hchiam@users.noreply.github.com>
2021-05-10 01:27:55 -0400

    clipboard event (still a WIP)

--- app/components/metatip/ally.element.js
+++ app/components/metatip/ally.element.js
@@ -1,12 +1,36 @@
+import $ from 'blingblingjs'
 import { Metatip } from './metatip.element.js'
+// import { Selectable } from '../../features/'
 
 export class Ally extends Metatip {
   constructor() {
     super()
+    // Selectable($('[color-swatch]', this.$shadow))
   }
 
-  copyColorSwatch() {
-    alert('copyColorSwatch')
+  copyToClipboard(text, callback) {
+    try {
+      var temp = document.createElement('textarea');
+      document.body.append(temp);
+      temp.value = text;
+      temp.select();
+      document.execCommand('copy');
+      temp.remove();
+      if (callback) callback(text);
+    } catch (err) {
+      alert(
+        'Could not automatically copy to clipboard. \n\n Copy this text instead: \n\n' +
+          text
+      );
+    }
+  }
+
+  clipboardMessage() {
+    alert('Supposed to copy to clipboard but getting error.');
+  }
+
+  copyColorSwatch(event) {
+    this.copyToClipboard(event.target.querySelector('span').innerText.trim(), this.clipboardMessage)
   }
 
   observe() {
