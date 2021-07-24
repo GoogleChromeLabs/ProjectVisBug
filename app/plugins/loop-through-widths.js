b0310fafc420e8de6e043a43939ad919d0e20e59
hchiam <hchiam@users.noreply.github.com>
2021-07-23 21:36:28 -0400

    loop-through-widths

---
+++ app/plugins/loop-through-widths.js
@@ -0,0 +1,74 @@
+// https://github.com/hchiam/learning-js/blob/master/bookmarklets/autoResizeWindowToTestMediaQueries.js
+
+export const commands = [
+  'loop through widths',
+  'loop thru widths',
+]
+
+export const description = 'loops through screen widths in a popup'
+
+export default async function() {
+  var test = (function () {
+    var keepGoing = true;
+    var popup;
+    var maxWidth = screen.width;
+    var minWidth = 152;
+    var goWider = true;
+
+    openPopup();
+    go();
+
+    popup.onbeforeunload = function () {
+      stop();
+      console.log("Stopped timer.");
+    };
+    function openPopup() {
+      popup = window.open(location.href, "_blank", "width=100, top=0, left=0");
+    }
+    function wider() {
+      popup.resizeBy(5, 0);
+    }
+    function thinner() {
+      popup.resizeBy(-5, 0);
+    }
+    function stop() {
+      keepGoing = false;
+    }
+    function go() {
+      keepGoing = true;
+      console.log("Popup will start looping thru screen widths in 3 seconds.");
+      popup.focus();
+      setTimeout(function () {
+        scanWidths();
+        const codeStyle = 'background: black; color: lime;'
+        const resetStyle = 'background: inherit; color: inherit;'
+        console.log(
+          "You can run %ctest.stop()%c to stop, and \n%ctest.go()%c to continue.",
+          codeStyle, resetStyle, codeStyle, resetStyle
+        );
+      }, 3000);
+    }
+    function scanWidths() {
+      var timer = setInterval(function () {
+        if (!keepGoing) {
+          clearTimeout(timer);
+          popup.focus();
+        }
+        if (maxWidth <= popup.outerWidth) {
+          goWider = false;
+        } else if (popup.outerWidth <= minWidth) {
+          goWider = true;
+        }
+        if (goWider) {
+          wider();
+        } else {
+          thinner();
+        }
+      }, 100);
+    }
+
+    return { stop, go, popup };
+  })();
+
+  if (typeof window !== "undefined") window.test = test;
+}
