6b94fd19985c4f14addbd78a9cecb537a949be0a
Tanner <tannercdolby@gmail.com>
2022-10-12 23:46:48 -0700

    Add the 'placeholdifier' plugin

---
+++ app/plugins/placeholdifier.js
@@ -0,0 +1,21 @@
+import { loadStyles } from '../utilities/styles.js'
+
+export const commands = [
+    'placeholdifier',
+]
+
+export const description = 'turn the page into a live wireframe'
+
+export default async function() {
+    await loadStyles(['https://unpkg.com/placeholdifier/placeholdifier.css'])
+
+    const ignored = ['script', 'link']
+    const body = document.querySelector('body')
+    const elements = Array.from(body.querySelectorAll('*'))
+        .filter(el => !ignored.includes(el.tagName?.toLowerCase()))
+    
+    elements.forEach(el => {
+        if (Array.from(el.classList).includes("placeholdify")) return;
+        el.classList.add('placeholdify');
+    })
+}
