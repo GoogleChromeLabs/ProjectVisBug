876e24bae291c2abbc69d67f8a3884d4665f701d
Tanner <tannercdolby@gmail.com>
2022-10-14 14:51:37 -0700

    Add optional chaining back as test runner was updated

--- app/plugins/placeholdifier.js
+++ app/plugins/placeholdifier.js
@@ -15,7 +15,7 @@ export default async function() {
     const ignored = ['script', 'link']
     const body = document.querySelector('body')
     const elements = Array.from(body.querySelectorAll('*'))
-        .filter(el => !ignored.includes(el.tagName.toLowerCase()))
+        .filter(el => !ignored.includes(el.tagName?.toLowerCase()))
     
     elements.forEach(el => {
         if (Array.from(el.classList).includes('placeholdify')) return;
