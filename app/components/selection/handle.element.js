a61d6103bd082aafa51588ad60f324aae4dddc8b
Adam Argyle <argyle@google.com>
2023-02-01 13:13:18 -0800

    fixes #589

--- app/components/selection/handle.element.js
+++ app/components/selection/handle.element.js
@@ -37,7 +37,7 @@ export class Handle extends HTMLElement {
     if (e.button !== 0) return
 
     const placement = this.placement
-    const handlesEl = e.path.find(el => el.tagName === 'VISBUG-HANDLES')
+    const handlesEl = e.composedPath().find(el => el.tagName === 'VISBUG-HANDLES')
     const nodeLabelId = handlesEl.getAttribute('data-label-id')
     const [sourceEl] = $(`[data-label-id="${nodeLabelId}"]`)
 
